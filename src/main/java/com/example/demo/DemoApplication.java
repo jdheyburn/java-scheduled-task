650f6b5e60f9acd5bfd642b1ae359bcf89e19b79
Joseph Heyburn <34041368+jdheyburn@users.noreply.github.com>
2020-07-30 09:04:09 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import java.io.File;
+import java.nio.file.Files;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.AbstractFileResolvingResource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@SpringBootApplication
+@EnableScheduling
+public class DemoApplication {
+
+	public static final String FILENAME = "weather.csv";
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Scheduled(fixedRate = 120000) // Every 2 mins
+	public void getWeatherContents() throws Exception {
+		AbstractFileResolvingResource resource = new ClassPathResource(FILENAME);
+		File file = resource.getFile();
+		String contents = new String(Files.readAllBytes(file.toPath()));
+		System.out.println(contents);
+	}
+
+}
